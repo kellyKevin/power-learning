25d20876b0a7455a16f0983b0ae5adb8a821c4c4
kelly momanyi <kellymomanyi80@gmail.com>
2024-02-24 16:28:35 +0300

    user_input.py

---
+++ user_input.py
@@ -0,0 +1,7 @@
+# Ask for user input
+name = input("What is your name? ")
+age = input("What is your age? ")
+location = input("Where are you located? ")
+
+# Print personalized message
+print("Hello " + name + ", you are " + age + " years old and live in " + location + ".")
