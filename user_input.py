5797390fa9fc28eec69b2ee9ea5d751b21e9d2d1
kelly momanyi <kellymomanyi80@gmail.com>
2024-03-27 20:24:30 +0300

    first commit

--- user_input.py
+++
@@ -1,7 +0,0 @@
-# Ask for user input
-name = input("What is your name? ")
-age = input("What is your age? ")
-location = input("Where are you located? ")
-
-# Print personalized message
-print("Hello " + name + ", you are " + age + " years old and live in " + location + ".")
